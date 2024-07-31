d3841de6410efeee08ec06b19a86cf2f8f757a70
Piotr Bahlaj <piotr.bahlaj@o2.pl>
2024-07-31 14:17:33 +0200

    Create splash screen setup for both android 12+ and iOS

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -9,6 +9,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(context.localizations.welcome),
       ),
       body: Center(
