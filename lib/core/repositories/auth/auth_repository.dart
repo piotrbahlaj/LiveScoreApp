912c1c4ac8c497a478169778c921386378272295
Piotr Bahlaj <piotr.bahlaj@o2.pl>
2024-08-06 09:55:06 +0200

    Fix auth repo

--- lib/core/repositories/auth/auth_repository.dart
+++ lib/core/repositories/auth/auth_repository.dart
@@ -9,15 +9,11 @@ class AuthRepository implements AuthRepositoryInterface {
   @override
   Future<User?> signUpWithEmailAndPassword(
       String email, String password) async {
-    try {
-      UserCredential credential = await _auth.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      return credential.user;
-    } on FirebaseAuthException catch (e) {
-      throw FirebaseAuthException(code: e.code);
-    }
+    UserCredential credential = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return credential.user;
   }
 
   @override
